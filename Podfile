1408989163973995345fe532d7e10d799cd8daca
Mohamed Samir <mohamedsamirsalem100@gmail.com>
2019-10-11 16:42:52 +0200

    .

---
+++ Podfile
@@ -0,0 +1,13 @@
+ platform :ios, '9.0'
+
+target 'projectV5' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for projectV5
+	pod 'Alamofire'
+	pod 'IQKeyboardManager'
+	pod 'MOLH'
+	pod 'Kingfisher' 
+
+end
---
+++ projectV5/Controller/Foods/PopularTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PopularTableViewController.swift
+//  projectV5
+//
+//  Created by Mohamed Samir on 10/5/19.
+//  Copyright © 2019 Mohamed Samir. All rights reserved.
+//
+
+import UIKit
+
+class PopularTableViewController: UITableViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
